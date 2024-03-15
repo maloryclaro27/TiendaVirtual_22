163092a11337e603486c023c692eeb5c927de04e
cisco <Kquintero871@unab.edu.co>
2024-03-15 18:45:50 -0500

    15/03/24  trabajo en clase

--- app/src/main/java/com/nathquintero/tiendavirtual/MainActivity.java
+++ app/src/main/java/com/nathquintero/tiendavirtual/MainActivity.java
@@ -1,14 +1,28 @@
 package com.nathquintero.tiendavirtual;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<Product> listproducts= new ArrayList<>();
+    private ArrayList<Product> listproducts = new ArrayList<>();
+    private RecyclerView rvProductsMain;
+
+    private Toolbar topAppBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +30,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loadFakeData();
+
+        rvProductsMain = findViewById(R.id.rv_products_main);
+        topAppBar = findViewById(R.id.top_app_bar_list_product);
+        ProductAdapter myAdapter = new ProductAdapter(listproducts);
+        rvProductsMain.setAdapter(myAdapter);
+
+        topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.item_add_product) {
+                    startActivity(new Intent(MainActivity.this, FormProductActivity.class));
+                    return true;
+                } else if (item.getItemId() == R.id.item_add_category) {
+                    Toast.makeText(MainActivity.this, "Clic e}n ADD CATEGORY", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        GridLayoutManager myGrid = new GridLayoutManager(getApplicationContext(), 1);
+
+        LinearLayoutManager myLinear = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        StaggeredGridLayoutManager myStagger = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+
+
+        rvProductsMain.setLayoutManager(myGrid);
+
     }
 
-    private void loadFakeData(){
-        Product producto1= new Product("Gafas de sol","Oklay, para hombre", 75000.0,"https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
-        Product producto2= new Product("Gafas de sol","Calvin Klein, para hombre", 65000.0,"https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
-        Product producto3= new Product("Gafas de sol","Carolina Herrera, para dama", 75000.0,"https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
-        Product producto4= new Product("Gafas de sol","Balenciaga, para dama", 55000.0,"https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
-        Product producto5= new Product("Gafas de sol","Versache, para dama", 80000.0,"https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
+    private void loadFakeData() {
+        Product producto1 = new Product("Gafas de sol", "Oklay, para hombre", 75000.0, "https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
+        Product producto2 = new Product("Gafas de sol", "Calvin Klein, para hombre", 65000.0, "https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
+        Product producto3 = new Product("Gafas de sol", "Carolina Herrera, para dama", 75000.0, "https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
+        Product producto4 = new Product("Gafas de sol", "Balenciaga, para dama", 55000.0, "https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
+        Product producto5 = new Product("Gafas de sol", "Versache, para dama", 80000.0, "https://drive.google.com/file/d/1G4MpvvEdY3emIbtN0reeI8deMiSVVA0t/view?usp=sharing");
         listproducts.add(producto1);
         listproducts.add(producto2);
         listproducts.add(producto3);
--- app/src/main/java/com/nathquintero/tiendavirtual/ProductAdapter.java
+++ app/src/main/java/com/nathquintero/tiendavirtual/ProductAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,10 +13,10 @@ import java.util.ArrayList;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
 
-    private ArrayList<ProductAdapter> listObjects;
+    private ArrayList<Product> listObjects;
 
-    public ProductAdapter(ArrayList<Product> listObjects){
-        this.listObjects= listObjects;
+    public ProductAdapter(ArrayList<Product> listObjects) {
+        this.listObjects = listObjects;
     }
 
     @NonNull
@@ -23,31 +24,36 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     public ProductAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View myView = LayoutInflater
                 .from(parent.getContext())
-                .from(R.layout.item_product,parent,false);
+                .inflate(R.layout.item_product,parent,false);
         return new ViewHolder(myView);
 
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductAdapter.ViewHolder holder, int position) {
-
+        holder.associate(listObjects.get(position));
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount(){
+        return listObjects.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private ImageView myImage;
-        private ImageView nameProduct, priceProduct;
+        private TextView nameProduct, priceProduct;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             myImage= itemView.findViewById(R.id.iv_product);
             nameProduct= itemView.findViewById(R.id.tv_name_product_item);
             priceProduct= itemView.findViewById(R.id.tv_price_product_item);
+        }
+
+        public void associate(Product myProduct) {
+            nameProduct.setText(myProduct.getName());
+            priceProduct.setText(myProduct.getPrice().toString());
         }
     }
 }
